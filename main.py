417731207d37746e9050708181c2717def0b9717
Juan Pablo Muñoz <66917960+juampamunoz@users.noreply.github.com>
2021-03-07 10:48:59 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 import selenium.common.exceptions as exceptions
+import pyautogui as robot
 import time
 import pandas as pd
 
@@ -15,6 +16,8 @@ linkedin_password = "Neural123456!"
 usuarios_con_datos1= "/html/body/div[7]/div[3]/div/div[2]/div/div/div/div[2]/ul/li["
 usuarios_con_datos2= "]/div/div/div[2]/div[1]/div/div[1]/span/div/span[1]/span/a"
 XP_info="/html/body/div[7]/div[3]/div/div/div/div/div[2]/div/main/div/div[1]/section/div[2]/div[2]/div[1]/ul[2]/li[3]/a/span"
+        #/html/body/div[8]/div[3]/div/div/div/div/div[2]/div/main/div/div[1]/section/div[2]/div[2]/div[1]/ul[2]/li[3]/a/span
+posicionSig = 827 , 404
 
 #Variables
 XP=""
@@ -27,6 +30,9 @@ Email = "N/A"
 Telf = "N/A"
 datos = []
 
+def robotClic(PosX,PosY):
+    robot.moveTo(PosX,PosY)
+    robot.click(clicks=1)
 
 def linkedin_login():
     global driver
@@ -63,66 +69,51 @@ def busqueda_perfil():
     linkedin_search=""
     search_term = input("Que perfiles desea buscar?: ")
     search_term = search_term.split()
+    places = input("En que lugar desea buscar (Si no desea un lugar específico ingrese NA): ")
     for term in search_term:
         linkedin_search = linkedin_search+term + "%20"
     linkedin_search_baseurl="http://www.linkedin.com/search/results/people/?keywords="
     driver.get(linkedin_search_baseurl + linkedin_search)
     time.sleep(3)
+    if places != "NA":
+        #MoverMouseUbicación
+        robotClic(437,197)
+        #MoverVentanadeTexto
+        robotClic(362,263)
+        robot.typewrite(places)
+        time.sleep(1.5)
+        #Seleccionar
+        robotClic(366,288)
+        #Buscar
+        robotClic(465,576)
+        time.sleep(3)
 
-def navegacion_perfiles():
+
+def conectar():
+    Xpa1="/html/body/div[8]/div[3]/div/div[1]/div/div/main/div/div/div[2]/ul/li["
+    Xpa2="]/div/div/div[3]/button"
+
     for i in range(10):
-        XP="/html/body/div[7]/div[3]/div/div[2]/div/div/div/div[3]/ul/li[{}]/div/div/div[2]/div[1]/div/div[1]/span/div/span[1]/span/a".format(i + 1)
-        #XP="/html/body/div[8]/div[3]/div/div[2]/div/div/div/div[2]/ul/li[{}]/div/div/div[2]/div[1]/div/div[1]/span/div/span[1]/span/a".format(i + 1)
-        Perfil = driver.find_element_by_xpath(XP).text
-        if Perfil != "Miembro de LinkedIn":
-            print("Perfil accesible")
-            driver.find_element_by_xpath(XP).click()
-            driver.implicitly_wait(3)
-            driver.find_element_by_xpath(XP_info).click()
-            Nombre = driver.find_element_by_xpath("/html/body/div[4]/div/div/div[1]/h1").text
+        for j in range(10):
+            XP_con=Xpa1 + str(j) + Xpa2
+            try:
+                driver.find_element_by_xpath(XP_con).click()
+            except exceptions.NoSuchElementException:
+                print("No existe el elemento: " + str(j))
 
-            #Tomar la info
-            for i in range(7):
-                if i == 0:
-                    try:
-                        header = driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]/section/div/div[1]/div/section/header")
-                        print("Encabezado: " + header.text)
-                        PerfilLink = driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]/section/div/div[1]/div/section/div/a").text
-                    except exceptions.NoSuchElementException:
-                        print("Existe más de 1 elemento")
-                else:
-                    try:
-                        header = driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]/section/div/div[1]/div/section[{}]/header".format(i+i))
-                        print("Encabezado:" + header.text)
-                        if header.text == "Email":
-                            print("Correo asignado")
-                            #Email = driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]/section/div/div[1]/div/section[{}]/div/a".format(i+1)).text
-                            #print(Email)
-                        elif header.text == "Número de teléfono":
-                            print("Teléfono asignado")
-                            #Telf = driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]/section/div/div[1]/div/section[{}]/div/a".format(i+1)).text
-                            #print(Telf)
+        time.sleep(3)
+        robot.hotkey('end')
+        time.sleep(1)
+        if i == 0:
+            robot.moveTo(827, 380)
+            robot.click(clicks=1)
+        else:
+            robot.moveTo(posicionSig)
+            robot.click(clicks=1)
 
-                    except exceptions.NoSuchElementException:
-                        print("No existen más datos")
 
-                #try:
-                #    header = driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]/section/div/div[1]/div/section[{}]/header".format(i+i))
-                #    print("Encabezado: " + header.text)
-                #except exceptions.NoSuchElementException:
-                #    print("No existen más datos")
-            datos = [Nombre, PerfilLink, Telf, Email]
-            Email = "N/A"
-            Telf = "N/A"
-            print(datos)
-
-            time.sleep(3)
-            driver.back()
-            time.sleep(3)
-            driver.back()
-            time.sleep(3)
 
 
 linkedin_login()
 busqueda_perfil()
-navegacion_perfiles()
+#conectar()
